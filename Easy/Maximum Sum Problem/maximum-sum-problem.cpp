9abce3c8779a465266348948fa0007cfd298c3a4
Siddharth Singh <97836894+siddharthSingh2003@users.noreply.github.com>
2024-02-24 23:15:44 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Maximum Sum Problem/maximum-sum-problem.cpp
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+        int maxSum(int n)
+        {
+            //code here.
+            if(int(n/2) + int(n/3) + int(n/4)<=n)
+                return n;
+            return maxSum(int(n/2))+maxSum(int(n/3))+maxSum(int(n/4));
+        }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        Solution ob;
+        cout<<ob.maxSum(n)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
